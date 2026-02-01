6f77fd1b6b8731dd97085b6bcd3561eea36c2696
kotmari <mariannakot79@gmail.com>
2026-02-01 18:40:09 +0200

    implement better-auth

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import subjectRouter from './db/routes/subjects'
 import cors from 'cors'
 import securityMiddleware from './middleware/security'
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app = express()
 const PORT = 8000
@@ -13,6 +15,8 @@ app.use(cors({
    methods: ['GET', 'POST', 'PUT', 'DELETE'],
    credentials: true
 }))
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json())
 app.use(securityMiddleware)
